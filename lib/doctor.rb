6a3e488107a69077c6e977ec5f747563bc519f20
Catherine Gloria <catherinegloria@gmail.com>
2020-10-23 13:53:50 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,13 @@ class Doctor
   end
 
   def new_appointment(date, patient, appointment)
-    
+    the_appointment = 
+    self.appointment = 
+  end
+
+  def new_song(title, genre)
+    song = Song.new(title, self, genre)
+    self.song = song
   end
 
 end
