c317c3b23e5b618c8ed7688c8476ec9c147fdd4d
Catherine Gloria <catherinegloria@gmail.com>
2020-10-23 14:45:41 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,16 +14,16 @@ class Doctor
   end
 
   def new_appointment(date, patient)
-    the_appt = Appointment.new(date, patient, self) #check on Appointment.new requirements
+    the_appt = Appointment.new(date, patient, self)
     self.appointment = the_appt
   end
 
   def appointments
-    Appointment.all.select {|appts| appts.doctor == self} #check on Appointment.new requirements
+    Appointment.all.select {|appts| appts.doctor == self}
   end
 
   def patients
-    appointments.collect {|my_appts| my_appts.patient} #check on Appointment.new requirements
+    appointments.collect {|my_appts| my_appts.patient}
   end
 
 end
